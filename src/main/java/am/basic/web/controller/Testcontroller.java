ba6b19e6b17ad5566137422f0cd2bbdee9d4df1d
ruben <rub095088881@gmail.com>
2020-02-13 19:31:13 +0400

    initialcommit0902

--- src/main/java/am/basic/web/controller/Testcontroller.java
+++ src/main/java/am/basic/web/controller/Testcontroller.java
@@ -5,9 +5,7 @@ import am.basic.web.repository.UserRepository;
 import am.basic.web.service.Userservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpSession;
@@ -28,7 +26,11 @@ public class Testcontroller {
 
 
     @RequestMapping(method = RequestMethod.POST, path = "/login")
-    public ModelAndView login(@RequestParam String username, @RequestParam("pass") String password, HttpSession session) throws SQLException {
+    public ModelAndView login(@RequestParam String username, @RequestParam("pass") String password,
+                              HttpSession session, @CookieValue(name = "username",value = "surname",required = false) String str)
+                                throws SQLException {
+
+
         User user = userRepository.getByUsernameAndPassword(username, password);
         if (user != null) {
             session.setAttribute("ttghg", user);
@@ -36,6 +38,29 @@ public class Testcontroller {
         }
 
         return new ModelAndView("index", "abcd", "wrong username or password");
+
+    }
+    @RequestMapping(method = RequestMethod.POST, path = "/registration")
+
+    public ModelAndView addUser(@RequestParam String name,
+                                @RequestParam String surname,
+                                @RequestParam String username,
+                                @RequestParam String age,
+                                @RequestParam String code,
+                                @RequestParam String password, HttpSession session) throws SQLException {
+        User user = new User();
+
+        user.setName(name);
+        user.setSurname(surname);
+        user.setUsername(username);
+        user.setAge(age);
+        user.setCode(code);
+        user.setPassword(password);
+        userRepository.add(user);
+        session.setAttribute("ttghg", user);
+
+        return new ModelAndView("registr", "abcd", "THe user added");
+
 
     }
 
